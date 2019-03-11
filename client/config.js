159527a24d15b6eb3dfb61cd3390b5ddaa226f4e
Håvar Aambø Fosstveit <havar@fosstveit.net>
2019-03-11 15:07:20 +0100

    Updated coturn for both TCP and TLS

--- client/config.js
+++ client/config.js
@@ -3,5 +3,10 @@ var iceServers = [
 		urls       : 'turn:XXXX-XXXX:443?transport=tcp',
 		username   : 'websip',
 		credential : 'websip'
+	},
+	{
+		urls       : 'turns:XXXX-XXXX:80?transport=tcp',
+		username   : 'websip',
+		credential : 'websip'
 	}
 ];
